2355884d38e2b97eadf38ade00db77ba937b251a
Stricted <info@stricted.net>
2018-01-30 23:20:12 +0100

    remove unneeded if statements
    Change-Id: Ia359b01178101ae2355030aaf3acfcb5b4fc35bd

--- jobs/twrp-build-kicker.groovy
+++ jobs/twrp-build-kicker.groovy
@@ -7,18 +7,10 @@ def jsonParse(def json) { new groovy.json.JsonSlurperClassic().parseText(json) }
 node("master"){
   def json = jsonParse(getDevices())
   for(int i = 0; i < json.size(); i++) {
-    if(device) {
-      if(device != json[i].device) {
-        if(version != json[i].version) {
-          continue
-        }
-        continue
-      }
-    }
     echo "Kicking off a build for ${json[i].device}"
     build job: 'twrp-build', parameters: [
       string(name: 'VERSION', value: (json[i].version == null) ? "7.1" : json[i].version),
-      string(name: 'DEVICE', value: (json[i].device == null) ? "HELP-omgwtfbbq" : json[i].device),
+      string(name: 'DEVICE', value: (json[i].device == null) ? "HELP-omgwtfbbq" : json[i].device)
     ], propagate: false, wait: false
     sleep 2
   }
