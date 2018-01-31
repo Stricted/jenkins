82ff1a443bf3ca8bc6ff4afef8db6f7899eb3ead
Stricted <info@stricted.net>
2018-01-31 05:07:11 +0100

    replace spaces with tabs
    this makes my life easier
    
    Change-Id: I62d690de06214ab3550c388928c25b94a07ae79f

--- jobs/lineage-build-kicker.groovy
+++ jobs/lineage-build-kicker.groovy
@@ -7,34 +7,34 @@ def jsonParse(def json) { new groovy.json.JsonSlurperClassic().parseText(json) }
 def global_repopick_nums = null
 def global_repopick_tops = null
 
-node("master"){
-timestamps {
-stage('Start Builds'){
-  def json = jsonParse(getDevices())
-  for(int i = 0; i < json.size(); i++) {
-    if(json[i].device == "GLOBAL") {
-      global_repopick_nums = json[i].repopick_nums
-      global_repopick_tops = json[i].repopick_tops
-      continue
-    }
-    echo "Kicking off a build for ${json[i].device}"
-    build job: 'lineage-build', parameters: [
-      string(name: 'VERSION', value: (json[i].version == null) ? "14.1" : json[i].version),
-      string(name: 'DEVICE', value: (json[i].device == null) ? "HELP-omgwtfbbq" : json[i].device),
-      string(name: 'BUILD_TYPE', value: (json[i].build_type == null) ? "userdebug" : json[i].build_type),
-      string(name: 'REPOPICK_NUMBERS', value: (json[i].repopick_nums == null) ? "" : json[i].repopick_nums),
-      string(name: 'REPOPICK_TOPICS', value: (json[i].repopick_tops == null) ? "" : json[i].repopick_tops),
-      string(name: 'GLOBAL_REPOPICK_NUMBERS', value: (global_repopick_nums == null) ? "" : global_repopick_nums),
-      string(name: 'GLOBAL_REPOPICK_TOPICS', value: (global_repopick_tops == null) ? "" : global_repopick_tops),
-      string(name: 'WITH_SU', value: (json[i].with_su == null) ? "false" : json[i].with_su),
-      string(name: 'WITH_DEXPREOPT', value: (json[i].with_dexpreopt == null) ? "false" : json[i].with_dexpreopt),
-      string(name: 'OTA', value: (json[i].ota == null) ? "true" : json[i].ota),
-      string(name: 'SIGNED', value: (json[i].signed == null) ? "false" : json[i].signed),
-      string(name: 'SIGNED_BACKUPTOOL', value: (json[i].signed_backuptool == null) ? "true" : json[i].signed_backuptool),
-      string(name: 'CLEAN', value: "true")
-    ], propagate: false, wait: false
-    sleep 2
-  }
-}
-}
+node("master") {
+	timestamps {
+		stage('Start Builds') {
+			def json = jsonParse(getDevices())
+			for(int i = 0; i < json.size(); i++) {
+				if(json[i].device == "GLOBAL") {
+					global_repopick_nums = json[i].repopick_nums
+					global_repopick_tops = json[i].repopick_tops
+					continue
+				}
+				echo "Kicking off a build for ${json[i].device}"
+				build job: 'lineage-build', parameters: [
+					string(name: 'VERSION', value: (json[i].version == null) ? "14.1" : json[i].version),
+					string(name: 'DEVICE', value: (json[i].device == null) ? "HELP-omgwtfbbq" : json[i].device),
+					string(name: 'BUILD_TYPE', value: (json[i].build_type == null) ? "userdebug" : json[i].build_type),
+					string(name: 'REPOPICK_NUMBERS', value: (json[i].repopick_nums == null) ? "" : json[i].repopick_nums),
+					string(name: 'REPOPICK_TOPICS', value: (json[i].repopick_tops == null) ? "" : json[i].repopick_tops),
+					string(name: 'GLOBAL_REPOPICK_NUMBERS', value: (global_repopick_nums == null) ? "" : global_repopick_nums),
+					string(name: 'GLOBAL_REPOPICK_TOPICS', value: (global_repopick_tops == null) ? "" : global_repopick_tops),
+					string(name: 'WITH_SU', value: (json[i].with_su == null) ? "false" : json[i].with_su),
+					string(name: 'WITH_DEXPREOPT', value: (json[i].with_dexpreopt == null) ? "false" : json[i].with_dexpreopt),
+					string(name: 'OTA', value: (json[i].ota == null) ? "true" : json[i].ota),
+					string(name: 'SIGNED', value: (json[i].signed == null) ? "false" : json[i].signed),
+					string(name: 'SIGNED_BACKUPTOOL', value: (json[i].signed_backuptool == null) ? "true" : json[i].signed_backuptool),
+					string(name: 'CLEAN', value: "true")
+				], propagate: false, wait: false
+				sleep 2
+			}
+		}
+	}
 }
--- jobs/twrp-build.groovy
+++ jobs/twrp-build.groovy
@@ -3,66 +3,66 @@ String calcDate() { ['date', '+%Y%m%d'].execute().text.trim()}
 def BUILD_TREE  = "/home/build/android/twrp/" + VERSION
 def CCACHE_DIR  = "/home/build/.ccache"
 
-node("build"){
-  timestamps {
-    currentBuild.displayName = 'twrp-' + DEVICE
+node("build") {
+	timestamps {
+		currentBuild.displayName = 'twrp-' + DEVICE
 
-    stage('Input manifest'){
-      sh '''#!/bin/bash
-        cd '''+BUILD_TREE+'''
-        rm -rf .repo/local_manifests
-        mkdir .repo/local_manifests
-        if ! [[ -z "$MANIFEST" ]]; then
-          curl --silent "https://raw.githubusercontent.com/Stricted/jenkins/Stricted/manifests/twrp-$VERSION/$MANIFEST.xml" > .repo/local_manifests/roomservice.xml
-        else
-          curl --silent "https://raw.githubusercontent.com/Stricted/jenkins/Stricted/manifests/twrp-$VERSION/$DEVICE.xml" > .repo/local_manifests/roomservice.xml
-        fi
-      '''
-    }
-    stage('Sync'){
-      sh '''#!/bin/bash
-        set +x
-        cd '''+BUILD_TREE+'''
-        export GIT_SSH_COMMAND="ssh -o ControlPath=none"
-        repo sync -d -c -j128 --force-sync
-      '''
-    }
-    stage('Clean'){
-      if(CLEAN == 'true'){
-        sh '''#!/bin/bash
-          cd '''+BUILD_TREE+'''
-          make clean
-        '''
-      }
-      else {
-        sh '''#!/bin/bash
-          echo "skip cleaning";
-        '''
-      }
-    }
-    stage('Build'){
-      sh '''#!/bin/bash +e
-        cd '''+BUILD_TREE+'''
-        if [[ $VERSION = '8.1' ]]; then
-          export ALLOW_MISSING_DEPENDENCIES=true;
-        fi
-        . build/envsetup.sh
-        export USE_CCACHE=1
-        export CCACHE_COMPRESS=1
-        export CCACHE_DIR='''+CCACHE_DIR+'''
-        lunch omni_$DEVICE-eng
-        mka recoveryimage
-      '''
-    }
-    stage('Upload'){
-      sh '''#!/bin/bash
-        set -e
-        cp '''+BUILD_TREE+'''/out/target/product/$DEVICE/recovery.img twrp-$DEVICE.img
-      '''
-      archiveArtifacts artifacts: '*'
-      sh '''#!/bin/bash
-        rm *
-      '''
-    }
-  }
+		stage('Input manifest') {
+		sh '''#!/bin/bash
+			cd '''+BUILD_TREE+'''
+			rm -rf .repo/local_manifests
+			mkdir .repo/local_manifests
+			if ! [[ -z "$MANIFEST" ]]; then
+				curl --silent "https://raw.githubusercontent.com/Stricted/jenkins/Stricted/manifests/twrp-$VERSION/$MANIFEST.xml" > .repo/local_manifests/roomservice.xml
+			else
+				curl --silent "https://raw.githubusercontent.com/Stricted/jenkins/Stricted/manifests/twrp-$VERSION/$DEVICE.xml" > .repo/local_manifests/roomservice.xml
+			fi
+			'''
+		}
+		stage('Sync') {
+			sh '''#!/bin/bash
+				set +x
+				cd '''+BUILD_TREE+'''
+				export GIT_SSH_COMMAND="ssh -o ControlPath=none"
+				repo sync -d -c -j128 --force-sync
+			'''
+		}
+		stage('Clean') {
+			if(CLEAN == 'true') {
+				sh '''#!/bin/bash
+					cd '''+BUILD_TREE+'''
+					make clean
+				'''
+			}
+			else {
+				sh '''#!/bin/bash
+					echo "skip cleaning";
+				'''
+			}
+		}
+		stage('Build') {
+			sh '''#!/bin/bash +e
+				cd '''+BUILD_TREE+'''
+				if [[ $VERSION = '8.1' ]]; then
+					export ALLOW_MISSING_DEPENDENCIES=true;
+				fi
+				. build/envsetup.sh
+				export USE_CCACHE=1
+				export CCACHE_COMPRESS=1
+				export CCACHE_DIR='''+CCACHE_DIR+'''
+				lunch omni_$DEVICE-eng
+				mka recoveryimage
+			'''
+		}
+		stage('Upload') {
+			sh '''#!/bin/bash
+				set -e
+				cp '''+BUILD_TREE+'''/out/target/product/$DEVICE/recovery.img twrp-$DEVICE.img
+			'''
+			archiveArtifacts artifacts: '*'
+			sh '''#!/bin/bash
+				rm *
+			'''
+		}
+	}
 }
