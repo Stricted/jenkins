0a9f5243678501a0ada26b987c3a524d61dfecc5
Stricted <info@stricted.net>
2018-04-15 13:39:40 +0200

    remove WITH_DEXPREOPT parameter
    Change-Id: Idb9ebdd2e7fc4f2b089d1b1b20674cbe5f1ec5dd

--- jobs/lineage-build-kicker.groovy
+++ jobs/lineage-build-kicker.groovy
@@ -27,7 +27,6 @@ node("master") {
 					string(name: 'GLOBAL_REPOPICK_NUMBERS', value: (global_repopick_nums == null) ? "" : global_repopick_nums),
 					string(name: 'GLOBAL_REPOPICK_TOPICS', value: (global_repopick_tops == null) ? "" : global_repopick_tops),
 					string(name: 'WITH_SU', value: (json[i].with_su == null) ? "false" : json[i].with_su),
-					string(name: 'WITH_DEXPREOPT', value: (json[i].with_dexpreopt == null) ? "false" : json[i].with_dexpreopt),
 					string(name: 'OTA', value: (json[i].ota == null) ? "true" : json[i].ota),
 					string(name: 'SIGNED', value: (json[i].signed == null) ? "false" : json[i].signed),
 					string(name: 'SIGNED_BACKUPTOOL', value: (json[i].signed_backuptool == null) ? "true" : json[i].signed_backuptool),
