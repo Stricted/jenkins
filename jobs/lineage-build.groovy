e0c7df8ca7d78cbb1b37e9370c2493d4cbeb9d53
Stricted <info@stricted.net>
2018-04-15 09:03:42 +0200

    Revert "send slack notifications"
    This reverts commit db83ef207947cba527488d4d96c76b34e74e0a5b.
    
    Change-Id: I4bb16904337c101b6f6ac42b5086a95e3cd6a61a

--- jobs/lineage-build.groovy
+++ jobs/lineage-build.groovy
@@ -6,174 +6,161 @@ def CERTS_DIR   = "/home/build/.android-certs"
 
 def basejobname = DEVICE + '-' + VERSION + '-' + calcDate() + '-' + BUILD_TYPE
 
-def slack
-node("master"){ slack = load "${workspace}@script/includes/notifySlack.groovy" }
-
 node("build") {
 	timestamps {
-		try {
+		if(SIGNED == 'true') {
+			basejobname = basejobname + '-signed'
+		}
+		if(OTA == 'true') {
+			currentBuild.displayName = basejobname
+		} else {
+			currentBuild.displayName = basejobname + '-priv'
+		}
+		if(BOOT_IMG_ONLY == 'true') {
+			OTA = false
+		}
+		stage('Input manifest') {
+			sh '''#!/bin/bash
+				cd '''+BUILD_TREE+'''
+				rm -rf .repo/local_manifests
+				mkdir .repo/local_manifests
+				curl --silent "https://raw.githubusercontent.com/Stricted/jenkins/Stricted/manifests/lineage-$VERSION/$DEVICE.xml" > .repo/local_manifests/roomservice.xml
+			'''
+		}
+		stage('Sync') {
+			sh '''#!/bin/bash
+				set +x
+				cd '''+BUILD_TREE+'''
+				export GIT_SSH_COMMAND="ssh -o ControlPath=none"
+				repo sync -d -c -j128 --force-sync
+			'''
+		}
+		stage('Repopicks') {
+			sh '''#!/bin/bash
+				cd '''+BUILD_TREE+'''
+				. build/envsetup.sh
+				if ! [ -z $GLOBAL_REPOPICK_NUMBERS ]; then
+					for rpnum in ${GLOBAL_REPOPICK_NUMBERS//,/ }; do
+						repopick -fr $rpnum
+					done
+				else
+					echo "No global repopick numbers chosen"
+				fi
+				if ! [ -z $GLOBAL_REPOPICK_TOPICS ]; then
+					for rptopic in ${GLOBAL_REPOPICK_TOPICS//,/ }; do
+						repopick -fr -t $rptopic
+					done
+				else
+					echo "No global repopick topics chosen"
+				fi
+				if ! [ -z $REPOPICK_NUMBERS ]; then
+					for rpnum in ${REPOPICK_NUMBERS//,/ }; do
+						repopick -fr $rpnum
+					done
+				else
+					echo "No repopick numbers chosen"
+				fi
+				if ! [ -z $REPOPICK_TOPICS ]; then
+					for rptopic in ${REPOPICK_TOPICS//,/ }; do
+						repopick -fr -t $rptopic
+					done
+				else
+					echo "No repopick topics chosen"
+				fi
+			'''
+		}
+		stage('Clean') {
+			if(CLEAN == 'true') {
+				sh '''#!/bin/bash
+					cd '''+BUILD_TREE+'''
+					make clean
+				'''
+			}
+			else {
+				echo("skip cleaning");
+			}
+		}
+		stage('Build') {
+			sh '''#!/bin/bash +e
+				cd '''+BUILD_TREE+'''
+				. build/envsetup.sh
+				export USE_CCACHE=1
+				export CCACHE_COMPRESS=1
+				export CCACHE_DIR='''+CCACHE_DIR+'''
+				lunch lineage_$DEVICE-$BUILD_TYPE || lunch cm_$DEVICE-$BUILD_TYPE
+				./prebuilts/sdk/tools/jack-admin list-server && ./prebuilts/sdk/tools/jack-admin kill-server
+				rm -rf ~/.jack*
+				./prebuilts/sdk/tools/jack-admin install-server ./prebuilts/sdk/tools/jack-launcher.jar ./prebuilts/sdk/tools/jack-server-*.jar
+				export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx6g"
+				./prebuilts/sdk/tools/jack-admin start-server
+				if [ $BOOT_IMG_ONLY = 'true' ]; then
+					mka bootimage
+				else
+					mka bacon
+				fi
+				./prebuilts/sdk/tools/jack-admin list-server && ./prebuilts/sdk/tools/jack-admin kill-server
+			'''
+		}
+		stage('Sign build') {
 			if(SIGNED == 'true') {
-				basejobname = basejobname + '-signed'
+				sh '''#!/bin/bash
+				set -e
+				cd '''+BUILD_TREE+'''
+				OtaScriptPath=$([ -f out/target/product/$DEVICE/ota_script_path ] && cat "out/target/product/$DEVICE/ota_script_path" || echo "build/tools/releasetools/ota_from_target_files")
+				rm -f out/target/product/$DEVICE/lineage-$VERSION-*.zip
+				./build/tools/releasetools/sign_target_files_apks -o -d '''+CERTS_DIR+''' \
+					out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/*target_files*.zip \
+					out/target/product/$DEVICE/jenkins-signed-target_files.zip
+				$OtaScriptPath -k '''+CERTS_DIR+'''/releasekey \
+					--block --backup=$SIGNED_BACKUPTOOL \
+					out/target/product/$DEVICE/jenkins-signed-target_files.zip \
+					out/target/product/$DEVICE/lineage-$VERSION-$(date +%Y%m%d)-UNOFFICIAL-$DEVICE-signed.zip
+				'''
 			}
+			else {
+				echo("skip signing");
+			}
+		}
+		stage('Upload') {
 			if(OTA == 'true') {
-				currentBuild.displayName = basejobname
-			} else {
-				currentBuild.displayName = basejobname + '-priv'
-			}
-			if(BOOT_IMG_ONLY == 'true') {
-				OTA = false
-			}
-			slack.notifySlack('STARTED')
-			stage('Input manifest') {
 				sh '''#!/bin/bash
-					cd '''+BUILD_TREE+'''
-					rm -rf .repo/local_manifests
-					mkdir .repo/local_manifests
-					curl --silent "https://raw.githubusercontent.com/Stricted/jenkins/Stricted/manifests/lineage-$VERSION/$DEVICE.xml" > .repo/local_manifests/roomservice.xml
+					if [ $OTA = 'true' ]; then
+						zipname=$(find '''+BUILD_TREE+'''/out/target/product/$DEVICE/ -name 'lineage-'$VERSION'-*.zip' -type f -printf "%f\\n")
+						ssh web52@stricted.net mkdir -p /var/www/web52/htdocs/lineageos/$DEVICE/
+						scp '''+BUILD_TREE+'''/out/target/product/$DEVICE/$zipname web52@stricted.net:/var/www/web52/htdocs/lineageos/$DEVICE/
+					else
+						echo "Skipping as this is not a production build. Artifacts will be available in Jenkins"
+					fi
 				'''
 			}
-			stage('Sync') {
+			else {
 				sh '''#!/bin/bash
-					set +x
-					cd '''+BUILD_TREE+'''
-					export GIT_SSH_COMMAND="ssh -o ControlPath=none"
-					repo sync -d -c -j128 --force-sync
-				'''
-			}
-			stage('Repopicks') {
-				sh '''#!/bin/bash
-					cd '''+BUILD_TREE+'''
-					. build/envsetup.sh
-					if ! [ -z $GLOBAL_REPOPICK_NUMBERS ]; then
-						for rpnum in ${GLOBAL_REPOPICK_NUMBERS//,/ }; do
-							repopick -fr $rpnum
-						done
-					else
-						echo "No global repopick numbers chosen"
-					fi
-					if ! [ -z $GLOBAL_REPOPICK_TOPICS ]; then
-						for rptopic in ${GLOBAL_REPOPICK_TOPICS//,/ }; do
-							repopick -fr -t $rptopic
-						done
-					else
-						echo "No global repopick topics chosen"
-					fi
-					if ! [ -z $REPOPICK_NUMBERS ]; then
-						for rpnum in ${REPOPICK_NUMBERS//,/ }; do
-							repopick -fr $rpnum
-						done
-					else
-						echo "No repopick numbers chosen"
-					fi
-					if ! [ -z $REPOPICK_TOPICS ]; then
-						for rptopic in ${REPOPICK_TOPICS//,/ }; do
-							repopick -fr -t $rptopic
-						done
-					else
-						echo "No repopick topics chosen"
-					fi
-				'''
-			}
-			stage('Clean') {
-				if(CLEAN == 'true') {
-					sh '''#!/bin/bash
-						cd '''+BUILD_TREE+'''
-						make clean
-					'''
-				}
-				else {
-					echo("skip cleaning");
-				}
-			}
-			stage('Build') {
-				sh '''#!/bin/bash +e
-					cd '''+BUILD_TREE+'''
-					. build/envsetup.sh
-					export USE_CCACHE=1
-					export CCACHE_COMPRESS=1
-					export CCACHE_DIR='''+CCACHE_DIR+'''
-					lunch lineage_$DEVICE-$BUILD_TYPE || lunch cm_$DEVICE-$BUILD_TYPE
-					./prebuilts/sdk/tools/jack-admin list-server && ./prebuilts/sdk/tools/jack-admin kill-server
-					rm -rf ~/.jack*
-					./prebuilts/sdk/tools/jack-admin install-server ./prebuilts/sdk/tools/jack-launcher.jar ./prebuilts/sdk/tools/jack-server-*.jar
-					export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx6g"
-					./prebuilts/sdk/tools/jack-admin start-server
-					if [ $BOOT_IMG_ONLY = 'true' ]; then
-						mka bootimage
-					else
-						mka bacon
-					fi
-					./prebuilts/sdk/tools/jack-admin list-server && ./prebuilts/sdk/tools/jack-admin kill-server
-				'''
-			}
-			stage('Sign build') {
-				if(SIGNED == 'true') {
-					sh '''#!/bin/bash
 					set -e
-					cd '''+BUILD_TREE+'''
-					OtaScriptPath=$([ -f out/target/product/$DEVICE/ota_script_path ] && cat "out/target/product/$DEVICE/ota_script_path" || echo "build/tools/releasetools/ota_from_target_files")
-					rm -f out/target/product/$DEVICE/lineage-$VERSION-*.zip
-					./build/tools/releasetools/sign_target_files_apks -o -d '''+CERTS_DIR+''' \
-						out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/*target_files*.zip \
-						out/target/product/$DEVICE/jenkins-signed-target_files.zip
-					$OtaScriptPath -k '''+CERTS_DIR+'''/releasekey \
-						--block --backup=$SIGNED_BACKUPTOOL \
-						out/target/product/$DEVICE/jenkins-signed-target_files.zip \
-						out/target/product/$DEVICE/lineage-$VERSION-$(date +%Y%m%d)-UNOFFICIAL-$DEVICE-signed.zip
-					'''
-				}
-				else {
-					echo("skip signing");
-				}
-			}
-			stage('Upload') {
-				if(OTA == 'true') {
-					sh '''#!/bin/bash
-						if [ $OTA = 'true' ]; then
-							zipname=$(find '''+BUILD_TREE+'''/out/target/product/$DEVICE/ -name 'lineage-'$VERSION'-*.zip' -type f -printf "%f\\n")
-							ssh web52@stricted.net mkdir -p /var/www/web52/htdocs/lineageos/$DEVICE/
-							scp '''+BUILD_TREE+'''/out/target/product/$DEVICE/$zipname web52@stricted.net:/var/www/web52/htdocs/lineageos/$DEVICE/
-						else
-							echo "Skipping as this is not a production build. Artifacts will be available in Jenkins"
-						fi
-					'''
-				}
-				else {
-					sh '''#!/bin/bash
-						set -e
-						if [ $BOOT_IMG_ONLY = 'true' ]; then
-							cp '''+BUILD_TREE+'''/out/target/product/$DEVICE/boot.img .
-						else
-							cp '''+BUILD_TREE+'''/out/target/product/$DEVICE/lineage-$VERSION-* .
-						fi
-					'''
-					archiveArtifacts artifacts: '*.zip'
-					sh '''#!/bin/bash
-						rm -rf *
-					'''
-				}
-			}
-			stage('Add to updater') {
-				withCredentials([string(credentialsId: '16c4643c-0dfb-4f0d-ad8a-54acccae6785', variable: 'UPDATER_API_KEY')]) {
-					sh '''#!/bin/bash
-						cd '''+BUILD_TREE+'''/out/target/product/$DEVICE
-						if [ $OTA = 'true' ]; then
-							LineageUpdaterURL="https://lineage.stricted.net"
-							DownloadBaseURL="https://images.stricted.net/lineageos"
-							zipname=$(find -name "lineage-$VERSION-*.zip" -type f -printf '%f\n')
-							md5sum=$(md5sum $zipname)
-							curl -H "Apikey: $UPDATER_API_KEY" -H "Content-Type: application/json" -X POST -d '{ "device": "'"$DEVICE"'", "filename": "'"$zipname"'", "md5sum": "'"${md5sum:0:32}"'", "romtype": "unofficial", "url": "'"$DownloadBaseURL/$DEVICE/$zipname"'", "version": "'"$VERSION"'" }' "$LineageUpdaterURL/api/v1/add_build"
-						fi
-					'''
-				}
+					if [ $BOOT_IMG_ONLY = 'true' ]; then
+						cp '''+BUILD_TREE+'''/out/target/product/$DEVICE/boot.img .
+					else
+						cp '''+BUILD_TREE+'''/out/target/product/$DEVICE/lineage-$VERSION-* .
+					fi
+				'''
+				archiveArtifacts artifacts: '*'
+				sh '''#!/bin/bash
+					rm *
+				'''
 			}
 		}
-		catch (e) {
-			currentBuild.result = "FAILED"
-			throw e
-		}
-		finally {
-			slack.notifySlack(currentBuild.result)
+		stage('Add to updater') {
+			withCredentials([string(credentialsId: '16c4643c-0dfb-4f0d-ad8a-54acccae6785', variable: 'UPDATER_API_KEY')]) {
+				sh '''#!/bin/bash
+					cd '''+BUILD_TREE+'''/out/target/product/$DEVICE
+					if [ $OTA = 'true' ]; then
+						LineageUpdaterURL="https://lineage.stricted.net"
+						DownloadBaseURL="https://images.stricted.net/lineageos"
+						zipname=$(find -name "lineage-$VERSION-*.zip" -type f -printf '%f\n')
+						md5sum=$(md5sum $zipname)
+						curl -H "Apikey: $UPDATER_API_KEY" -H "Content-Type: application/json" -X POST -d '{ "device": "'"$DEVICE"'", "filename": "'"$zipname"'", "md5sum": "'"${md5sum:0:32}"'", "romtype": "unofficial", "url": "'"$DownloadBaseURL/$DEVICE/$zipname"'", "version": "'"$VERSION"'" }' "$LineageUpdaterURL/api/v1/add_build"
+					fi
+				'''
+			}
 		}
 	}
 }
